36a2654e6d7650989759f1060a46353bc37b59c1
nazarchykur <nazarchykur@gmail.com>
2019-10-15 10:33:24 +0300

    format code creational pattern / prototype

---
+++ src/com/company/creational/prototype/shapes/Circle.java
@@ -0,0 +1,28 @@
+package com.company.creational.prototype.shapes;
+
+public class Circle extends Shape {
+
+    public int radius;
+
+    public Circle(){}
+
+    public Circle(Circle target){
+        super(target);
+        if (target != null){
+            this.radius = target.radius;
+        }
+    }
+
+
+    @Override
+    public Shape clone() {
+        return new Circle();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Circle) || !super.equals(o)) return false;
+        Circle circle = (Circle) o;
+        return circle.radius == radius;
+    }
+}
