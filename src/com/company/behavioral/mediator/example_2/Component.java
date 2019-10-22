91b1cfccaf4ccecbe0f5e50786bf4de3aa3341b6
nazarchykur <nazarchykur@gmail.com>
2019-10-22 13:11:34 +0300

    add example_2 behavioral pattern / mediator

---
+++ src/com/company/behavioral/mediator/example_2/Component.java
@@ -0,0 +1,7 @@
+package com.company.behavioral.mediator.example_2;
+
+public interface Component {
+    void setMediator(Mediator mediator);
+
+    String getName();
+}
---
+++ src/com/company/behavioral/mediator/example_2/Mediator.java
@@ -0,0 +1,30 @@
+package com.company.behavioral.mediator.example_2;
+
+import javax.swing.*;
+
+/**
+ * Common mediator interface.
+ */
+public interface Mediator {
+    void addNewNote(Note note);
+
+    void deleteNote();
+
+    void getInfoFromList(Note note);
+
+    void saveChanges();
+
+    void markNote();
+
+    void clear();
+
+    void sendToFilter(ListModel listModel);
+
+    void setElementsList(ListModel list);
+
+    void registerComponent(Component component);
+
+    void hideElements(boolean flag);
+
+    void createGUI();
+}
