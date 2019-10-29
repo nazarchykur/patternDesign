93ad612ea69385d2d74d17fcaa9cfccfa081fa1e
nazarchykur <nazarchykur@gmail.com>
2019-10-29 15:02:48 +0200

    add example_1 behavioral pattern / Template Method

---
+++ src/com/company/behavioral/template_method/example_1/Demo.java
@@ -0,0 +1,16 @@
+package com.company.behavioral.template_method.example_1;
+
+public class Demo {
+    public static void main(String[] args) {
+
+        HouseTemplate houseType = new WoodenHouse();
+
+        //using template method
+        houseType.buildHouse();
+        System.out.println("************");
+
+        houseType = new GlassHouse();
+
+        houseType.buildHouse();
+    }
+}
---
+++ src/com/company/behavioral/template_method/example_1/WoodenHouse.java
@@ -0,0 +1,15 @@
+package com.company.behavioral.template_method.example_1;
+
+public class WoodenHouse extends HouseTemplate {
+
+    @Override
+    public void buildWalls() {
+        System.out.println("Building Wooden Walls");
+    }
+
+    @Override
+    public void buildPillars() {
+        System.out.println("Building Pillars with Wood coating");
+    }
+
+}
